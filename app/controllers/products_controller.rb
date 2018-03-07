50ab1bc6517bafaa44fc2ba70ad77b950562acbe
sergiicherevko <sergii.cherevko@yahoo.com>
2018-03-06 19:43:19 -0600

    Added routes and methods to product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+  def get_first_product
+    product = Product.first
+    render json: {
+      name: product.name,
+      price: product.price,
+      description: product.description
+    }
+  end
+
+  def get_all_products
+    products = Product.all 
+
+    products_array = products.map do |product|
+    {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      description: product.description
+    }
+    end
+    render json: products_array.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get"/sergii_first_product" => "products#get_first_product"
+  get"/sergii_all_products" => "products#get_all_products"
 end
